0661af1c9a097365abc6e02e0465fa8e6b43be8b
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-06-27 13:37:47 +0530

    commit

--- toggler.js
+++ toggler.js
@@ -1,9 +1,7 @@
 const toggler = (...list) => {
-  // to track the cycle
   let current = -1;
   const length = list.length;
   return function () {
-    //moves to next element, resets to 0 when current > length
     current = (current + 1) % length;
     return list[current];
   };
